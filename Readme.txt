07785f41d283984437bf8dbf2aada458b773086e
zohaibirshad <zohaibi619@gmail.com>
2020-11-11 03:10:20 -0800

    initial commit

---
+++ Readme.txt
@@ -0,0 +1,5 @@
+This module is from a project for automatic appointment booking for clients. And Assign job to a specific Employee 
+Which is free that time.
+
+
+If you want to see my JS Skills you can check dashboard.php file it includes javascript code at last of the file.
--- routes/web.php
+++ routes/web.php
@@ -1,237 +1,37 @@
 <?php
+Route::get('/', function () { return redirect('/admin/home'); });
 
+// Authentication Routes...
+$this->get('login', 'Auth\LoginController@showLoginForm')->name('auth.login');
+$this->post('login', 'Auth\LoginController@login')->name('auth.login');
+$this->post('logout', 'Auth\LoginController@logout')->name('auth.logout');
 
+// Change Password Routes...
+$this->get('change_password', 'Auth\ChangePasswordController@showChangePasswordForm')->name('auth.change_password');
+$this->patch('change_password', 'Auth\ChangePasswordController@changePassword')->name('auth.change_password');
 
-// ************************************ ADMIN SECTION **********************************************
+// Password Reset Routes...
+$this->get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('auth.password.reset');
+$this->post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('auth.password.reset');
+$this->get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+$this->post('password/reset', 'Auth\ResetPasswordController@reset')->name('auth.password.reset');
 
-Route::get('/', 'Front\FrontendController@home')->name('front.home');
-
-Route::get('/', 'Admin\AdminController@error')->name('admin.error');
-
-
-Route::get('/create_store',function(){
-    return view('create_store');
-
+Route::group(['middleware' => ['auth'], 'prefix' => 'admin', 'as' => 'admin.'], function () {
+    Route::get('/home', 'HomeController@index');
+    Route::resource('roles', 'Admin\RolesController');
+    Route::post('roles_mass_destroy', ['uses' => 'Admin\RolesController@massDestroy', 'as' => 'roles.mass_destroy']);
+    Route::resource('users', 'Admin\UsersController');
+    Route::post('users_mass_destroy', ['uses' => 'Admin\UsersController@massDestroy', 'as' => 'users.mass_destroy']);
+    Route::resource('clients', 'Admin\ClientsController');
+    Route::post('clients_mass_destroy', ['uses' => 'Admin\ClientsController@massDestroy', 'as' => 'clients.mass_destroy']);
+	Route::get('get-employees', 'Admin\EmployeesController@GetEmployees');
+    Route::resource('employees', 'Admin\EmployeesController');
+    Route::post('employees_mass_destroy', ['uses' => 'Admin\EmployeesController@massDestroy', 'as' => 'employees.mass_destroy']);
+    Route::resource('working_hours', 'Admin\WorkingHoursController');
+    Route::post('working_hours_mass_destroy', ['uses' => 'Admin\WorkingHoursController@massDestroy', 'as' => 'working_hours.mass_destroy']);
+    Route::resource('appointments', 'Admin\AppointmentsController');
+    Route::post('appointments_mass_destroy', ['uses' => 'Admin\AppointmentsController@massDestroy', 'as' => 'appointments.mass_destroy']);
+	Route::resource('services', 'Admin\ServicesController');
+	Route::post('services_mass_destroy', ['uses' => 'Admin\ServicesController@massDestroy', 'as' => 'services.mass_destroy']);
+	
 });
-
-
-
-Route::post('/create_store1','CreateStoreController@CreateStore');
-
-
-Route::post('/checkstorename','CreateStoreController@checkStoreName');
-
-
-
-
-
-Route::group([
-
-    'prefix' => '{storename}'
-
-], function() {
-
-  View::composer(['*'],function($settings){
-  
-  $storename = \Route::current()->parameter('storename');
-  
-  $settings->with('gs', App\Models\Generalsetting::where('storename',$storename)->first());
-
-  $settings->with('seo', DB::table('seotools')->where('storename',$storename)->first());
-  $settings->with('categories', App\Models\Category::where([['status','=',1],['storename',$storename]])->get());   
-  
-
-  if (\Session::has('language')) 
-  {
-      $data = DB::table('languages')->where([['id',\Session::get('language')],['storename',$storename]])->first();
-    
-      if($data == null)
-      {
-        $data = DB::table('languages')->where([['is_default','=',1],['storename',$storename]])->first();
-      }
-
-      $data_results = file_get_contents(public_path().'/assets/languages/'.$data->file);
-      $lang = json_decode($data_results);
-      $settings->with('langg', $lang);
-  }
-  else
-  {
-      $data = DB::table('languages')->where([['is_default','=',1],['storename',$storename]])->first();
-      if($data)
-      {
-        $data_results = file_get_contents(public_path().'/assets/languages/'.$data->file);
-        $lang = json_decode($data_results);
-        $settings->with('langg', $lang);
-      }
-      
-  }  
-
-  if (!\Session::has('popup')) 
-  {
-      $settings->with('visited', 1);
-  }
-  \Session::put('popup' , 1);
-   
-});
-
-Route::prefix('admin')->group(function() {
-
-
- 
-
-  //------------ ADMIN LOGIN SECTION ------------
-
-
-
-  Route::get('/login', 'Admin\LoginController@showLoginForm')->name('admin.login');
-
-  Route::post('/login', 'Admin\LoginController@login')->name('admin.login.submit');
-
-  Route::get('/forgot', 'Admin\LoginController@showForgotForm')->name('admin.forgot');
-
-  Route::post('/forgot', 'Admin\LoginController@forgot')->name('admin.forgot.submit');
-
-  Route::get('/logout', 'Admin\LoginController@logout')->name('admin.logout');
-
-
-
-  //------------ ADMIN LOGIN SECTION ENDS ------------
-
-
-
-  //------------ ADMIN NOTIFICATION SECTION ------------
-
-
-
-  // User Notification
-
-  Route::get('/user/notf/show', 'Admin\NotificationController@user_notf_show')->name('user-notf-show');
-
-  Route::get('/user/notf/count','Admin\NotificationController@user_notf_count')->name('user-notf-count');
-
-  Route::get('/user/notf/clear','Admin\NotificationController@user_notf_clear')->name('user-notf-clear');
-
-  // User Notification Ends
-
-
-
-  // Order Notification
-
-  Route::get('/order/notf/show', 'Admin\NotificationController@order_notf_show')->name('order-notf-show');
-
-  Route::get('/order/notf/count','Admin\NotificationController@order_notf_count')->name('order-notf-count');
-
-  Route::get('/order/notf/clear','Admin\NotificationController@order_notf_clear')->name('order-notf-clear');
-
-  // Order Notification Ends
-
-
-
-  // Product Notification
-
-  Route::get('/product/notf/show', 'Admin\NotificationController@product_notf_show')->name('product-notf-show');
-
-  Route::get('/product/notf/count','Admin\NotificationController@product_notf_count')->name('product-notf-count');
-
-  Route::get('/product/notf/clear','Admin\NotificationController@product_notf_clear')->name('product-notf-clear');
-
-  // Product Notification Ends
-
-
-
-
-  //------------ ADMIN NOTIFICATION SECTION ENDS ------------
-
-
-
-  //------------ ADMIN DASHBOARD & PROFILE SECTION ------------
-
-  Route::get('/', 'Admin\DashboardController@index')->name('admin.dashboard');
-
-  Route::get('/dashboard', 'Admin\DashboardController@index')->name('admin.dashboard');
-  
-  Route::get('/data', 'Admin\DashboardController@getdata')->name('admin.data');
-  
-
-  Route::get('/profile', 'Admin\DashboardController@profile')->name('admin.profile');
-
-  Route::post('/profile/update', 'Admin\DashboardController@profileupdate')->name('admin.profile.update');
-
-  Route::get('/password', 'Admin\DashboardController@passwordreset')->name('admin.password');
-
-  Route::post('/password/update', 'Admin\DashboardController@changepass')->name('admin.password.update');
-
-  
-
-  Route::get('/analytics', 'Admin\DashboardController@analytics')->name('admin-analytics');
-
-  Route::post('/get-analytics', 'Admin\DashboardController@get_analytics')->name('admin-get-analytics');
-
-  //------------ ADMIN DASHBOARD & PROFILE SECTION ENDS ------------
-
-
-
-
-
-  //------------ ADMIN ORDER SECTION ------------
-
-  Route::group(['middleware'=> 'permissions:orders'],function(){
-
-    Route::get('/orders/datatables/{slug}', 'Admin\OrderController@datatables')->name('admin-order-datatables');
-
-  });
-
-  
-
-  //------------ ADMIN ORDER SECTION ENDS------------
-
-
-
-
-
-  //------------ ADMIN PRODUCT SECTION ------------
-
-Route::group(['middleware'=>'permissions:products'],function(){
-  Route::get('/products/datatables', 'Admin\ProductController@datatables')->name('admin-prod-datatables');
-  Route::get('/products/deactive/datatables', 'Admin\ProductController@deactivedatatables')->name('admin-prod-deactive-datatables');
-  Route::get('/products/catalogs/datatables', 'Admin\ProductController@catalogdatatables')->name('admin-prod-catalog-datatables');
-});
-
-
-
- //------------ ADMIN PRODUCT DISCUSSION SECTION ENDS ------------
-
-
-
-
-
-  //------------ ADMIN COUPON SECTION ------------
-
-
-
-
-  //------------ ADMIN PAGE SETTINGS SECTION ------------
-
-
-
- 
-  // PAGE SECTION ENDS
-
-
-
-// ************************************ FRONT SECTION ENDS**********************************************
-
-
-
-
-
-
-
-
-
-  });
-
-});
-
-
-
